281f14ea68f9774e8a94fb4494f32908d64a3dc5
dres0 <humberto.ferreiray@gmail.com>
2022-06-25 11:58:24 -0400

    seed de clientes agregado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do |i|
+    Client.create(name: "Cliente #{i+1}", phone: 99886655+i, name: "Cliente #{i+1}", email: "emplo#{i+1}@gmail.com")
+end
