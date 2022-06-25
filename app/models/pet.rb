4e0d88edec275cc4dc479ef95a7d045f214d5fa6
dres0 <humberto.ferreiray@gmail.com>
2022-06-25 10:49:42 -0400

    tablas listas

---
+++ app/models/pet.rb
@@ -0,0 +1,3 @@
+class Pet < ApplicationRecord
+  belongs_to :Client
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :pet_histories
+  resources :pets
   resources :clients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
